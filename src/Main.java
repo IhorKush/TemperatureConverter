9a6e55f97052c8c095eedf532d2c4044334ee94f
IhorKush <ihoryakushev88@gmail.com>
2025-12-27 20:34:43 +0200

    Add Maska.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Temperature Converter app.");
+        System.out.println("Maska kabaska");
     }
 }
